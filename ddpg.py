3772aa3719044d2906a5d371c917e13e4f3d6621
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-03-01 11:47:30 +0000

    changes for GoalEnv type.

--- ddpg.py
+++ ddpg.py
@@ -1,5 +1,6 @@
 import gym
-import ctm2_rl
+from gym.wrappers import FlattenDictWrapper
+import ctm2_envs
 import numpy as np
 
 from stable_baselines.ddpg.policies import MlpPolicy
@@ -14,36 +15,6 @@ from numpy import inf
 import yaml
 import os
 
-best_mean_reward, n_steps = -np.inf, 0
-
-
-def callback(_locals, _globals):
-    """
-    Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
-    :param _locals: (dict)
-    :param _globals: (dict)
-    """
-    global n_steps, best_mean_reward
-    # Print stats every 1000 calls
-    if (n_steps + 1) % 100 == 0:
-        # Evaluate policy performance
-        x, y = ts2xy(load_results(log_dir), 'timesteps')
-        if len(x) > 0:
-            mean_reward = np.mean(y[-100:])
-            print(x[-1], 'timesteps')
-            print(
-                "Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
-
-            # New best model, you could save the agent here
-            if mean_reward > best_mean_reward:
-                best_mean_reward = mean_reward
-                # Example for saving best model
-                print("Saving new best model")
-                _locals['self'].save(log_dir + 'best_model.pkl')
-    n_steps += 1
-    return True
-
-
 # Create log dir
 log_dir = "/tmp/gym/"
 os.makedirs(log_dir, exist_ok=True)
@@ -55,9 +26,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = config['gpu_id']
 
 # Create and wrap the environment
 env = gym.make('ctm2-continuous-v0')
-env = Monitor(env, log_dir, allow_early_resets=True)
+env = FlattenDictWrapper(env, ['observation', 'desired_goal', 'achieved_goal'])
 env = DummyVecEnv([lambda: env])
-
 # the noise objects for DDPG
 n_actions = env.action_space.shape[-1]
 
@@ -68,7 +38,7 @@ param_noise = AdaptiveParamNoiseSpec(ddpg_params['param_noise']['initial_stddev'
 action_noise = NormalActionNoise(mean=ddpg_params['action_noise']['mean'] + np.zeros(n_actions),
                                  sigma=ddpg_params['action_noise']['sigma'] * np.ones(n_actions))
 model = DDPG(MlpPolicy, env, gamma=ddpg_params['gamma'], memory_policy=ddpg_params['memory_policy'],
-             eval_env=ddpg_params['eval_env'], nb_train_steps=ddpg_params['nb_train_steps'],
+             eval_env=None, nb_train_steps=ddpg_params['nb_train_steps'],
              nb_rollout_steps=ddpg_params['nb_rollout_steps'],
              nb_eval_steps=ddpg_params['nb_eval_steps'], param_noise=param_noise, action_noise=action_noise,
              normalize_observations=ddpg_params['normalize_observations'], tau=ddpg_params['tau'],
@@ -84,4 +54,4 @@ model = DDPG(MlpPolicy, env, gamma=ddpg_params['gamma'], memory_policy=ddpg_para
              tensorboard_log=ddpg_params['tensorboard_log'], _init_setup_model=ddpg_params['_init_setup_model'],
              policy_kwargs=ddpg_params['policy_kwargs'])
 
-model.learn(total_timesteps=ddpg_params['total_timesteps'], callback=callback)
+model.learn(total_timesteps=ddpg_params['total_timesteps'], log_interval=1)
