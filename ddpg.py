1e07cf557fd4c3b443ab3de1dd40c3e3782294ec
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-03-04 16:54:31 +0000

    changes to use cmd_util robotics_make_env and requirements.txt for no errors

--- ddpg.py
+++ ddpg.py
@@ -6,6 +6,7 @@ import numpy as np
 from stable_baselines.ddpg.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.bench import Monitor
+from stable_baselines.common.cmd_util import make_robotics_env
 from stable_baselines.results_plotter import load_results, ts2xy
 
 from stable_baselines.ddpg.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
@@ -21,9 +22,7 @@ ddpg_params = config['ddpg']
 os.environ["CUDA_VISIBLE_DEVICES"] = config['gpu_id']
 
 # Create and wrap the environment
-env = gym.make('ctm2-continuous-v0')
-env = FlattenDictWrapper(env, ['observation', 'desired_goal', 'achieved_goal'])
-env = DummyVecEnv([lambda: env])
+env = make_robotics_env(env_id='ctm2-continuous-v0', seed=1, allow_early_resets=True)
 # the noise objects for DDPG
 n_actions = env.action_space.shape[-1]
 
@@ -31,8 +30,8 @@ param_noise = AdaptiveParamNoiseSpec(ddpg_params['param_noise']['initial_stddev'
                                      ddpg_params['param_noise']['desired_action_stddev'],
                                      ddpg_params['param_noise']['adoption_coefficient'])
 
-action_noise = NormalActionNoise(mean=ddpg_params['action_noise']['mean'] + np.zeros(n_actions),
-                                 sigma=ddpg_params['action_noise']['sigma'] * np.ones(n_actions))
+action_noise = NormalActionNoise(mean=env.action_space.high / 2 + env.action_space.low / 2,
+                                 sigma= env.action_space.high / 4 - env.action_space.low / 4)
 model = DDPG(MlpPolicy, env, gamma=ddpg_params['gamma'], memory_policy=ddpg_params['memory_policy'],
              eval_env=None, nb_train_steps=ddpg_params['nb_train_steps'],
              nb_rollout_steps=ddpg_params['nb_rollout_steps'],
@@ -50,4 +49,4 @@ model = DDPG(MlpPolicy, env, gamma=ddpg_params['gamma'], memory_policy=ddpg_para
              tensorboard_log=ddpg_params['tensorboard_log'], _init_setup_model=ddpg_params['_init_setup_model'],
              policy_kwargs=ddpg_params['policy_kwargs'])
 
-model.learn(total_timesteps=ddpg_params['total_timesteps'], log_interval=1)
+model.learn(total_timesteps=ddpg_params['total_timesteps'], log_interval=100)
