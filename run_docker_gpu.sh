e27af9bfaa1e1a2d3d38c528d8db4c725ed6e298
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-03-28 13:06:29 +0000

    fixed some issues.

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -4,6 +4,5 @@
 echo "Executing in the docker (gpu image) for experiment $1"
 
 docker run -d --runtime=nvidia -it --rm \
-  -p 5000:8888 -p 5001:6006\
   --mount src=$(pwd),target=/root/code/ctm2-drl,type=bind keshaviyengar/ctm2_drl\
   bash -c "cd /root/code/ctm2-drl/ && python3 run_experiment.py -e $1"
--- run_docker_tensorboard.sh
+++ run_docker_tensorboard.sh
@@ -2,10 +2,8 @@
 # Launch an experiment using the docker gpu image
 # http://priss:5001
 
-echo "Executing tensorboard in the docker (gpu image):"
-
-
 docker run --runtime=nvidia -it --rm \
+  -p 5000:8888 -p 5001:6006\
   --mount src=$(pwd),target=/root/code/ctm2-drl,type=bind keshaviyengar/ctm2_drl\
   bash -c "cd /root/code/ctm2-drl/ && tensorboard --host 0.0.0.0 --logdir='logs/'"
 
