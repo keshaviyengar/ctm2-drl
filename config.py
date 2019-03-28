899af5369d826231acd35bfa00136fcb491d6ea7
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-03-28 11:55:25 +0000

    changes to how running experiments.

--- config.py
+++ config.py
@@ -2,18 +2,17 @@ import os
 
 GPU_ID = 0
 
-# DDPG_DENSE_PARAMS
-DDPG_DENSE_LAUNCH_PARAMS = [
+DDPG_DENSE = [
     '--env', 'distal-1-continuous-dense-v0',
     '--extra_import', 'ctm2_envs',
     '--alg', 'her',
-    '--num_cpu', '3',
+    '--num_cpu', '1',
     '--num_timesteps', str(2.5e6),
     # env
     '--max_u', '1.',  # max absolute value of actions on different coordinates
     # ddpg
     '--layers', '1',  # number of layers in the critic/actor networks
-    '--hidden', '64',  # number of neurons in each hidden layers
+    '--hidden', '32',  # number of neurons in each hidden layers
     '--network_class', 'baselines.her.actor_critic:ActorCritic',
     '--Q_lr', '0.001',  # critic learning rate
     '--pi_lr', '0.001',  # actor learning rate
@@ -42,12 +41,50 @@ DDPG_DENSE_LAUNCH_PARAMS = [
     '--save_path', 'policies/her'
 ]
 
-# DDPG_SPARSE_PARAMS
-DDPG_HER_SPARSE_LAUNCH_PARAMS = [
+DDPG_SPARSE = [
     '--env', 'distal-1-continuous-sparse-v0',
     '--extra_import', 'ctm2_envs',
     '--alg', 'her',
-    '--num_cpu', '3',
+    '--num_cpu', '1',
+    '--num_timesteps', str(2.5e6),
+    # env
+    '--max_u', '1.',  # max absolute value of actions on different coordinates
+    # ddpg
+    '--layers', '1',  # number of layers in the critic/actor networks
+    '--hidden', '32',  # number of neurons in each hidden layers
+    '--network_class', 'baselines.her.actor_critic:ActorCritic',
+    '--Q_lr', '0.001',  # critic learning rate
+    '--pi_lr', '0.001',  # actor learning rate
+    '--buffer_size', str(int(1E3)),  # for experience replay
+    '--polyak', '0.95',  # polyak averaging coefficient
+    '--action_l2', '1.0',  # quadratic penalty on actions (before rescaling by max_u)
+    '--clip_obs', '200.',
+    '--scope', 'ddpg',  # can be tweaked for testing
+    '--relative_goals', 'False',
+    # training
+    '--n_cycles', '50',  # per epoch
+    '--rollout_batch_size', '1',  # per mpi thread
+    '--n_batches', '40',  # training batches per cycle
+    '--batch_size', '256',  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
+    '--n_test_rollouts', '50',  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
+    '--test_with_polyak', 'False',  # run test episodes with the target network
+    # exploration
+    '--random_eps', '0.3',  # percentage of time a random action is taken
+    '--noise_eps', '0.2',  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
+    # HER
+    '--replay_strategy', 'none',  # supported modes: future, none
+    '--replay_k', '4',  # number of additional goals used for replay, only used if off_policy_data=future
+    # normalization
+    '--norm_eps', '1',  # epsilon used for observation normalization
+    '--norm_clip', '5',  # normalized observations are cropped to this values
+    '--save_path', 'policies/her'
+]
+
+DDPG_HER_DENSE = [
+    '--env', 'distal-1-continuous-dense-v0',
+    '--extra_import', 'ctm2_envs',
+    '--alg', 'her',
+    '--num_cpu', '1',
     '--num_timesteps', str(2.5e6),
     # env
     '--max_u', '1.',  # max absolute value of actions on different coordinates
@@ -81,5 +118,41 @@ DDPG_HER_SPARSE_LAUNCH_PARAMS = [
     '--norm_clip', '5',  # normalized observations are cropped to this values
     '--save_path', 'policies/her'
 ]
-
-EXPERIMENTS = [DDPG_HER_SPARSE_LAUNCH_PARAMS]
+DDPG_HER_SPARSE = [
+    '--env', 'distal-1-continuous-sparse-v0',
+    '--extra_import', 'ctm2_envs',
+    '--alg', 'her',
+    '--num_cpu', '1',
+    '--num_timesteps', str(2.5e6),
+    # env
+    '--max_u', '1.',  # max absolute value of actions on different coordinates
+    # ddpg
+    '--layers', '1',  # number of layers in the critic/actor networks
+    '--hidden', '32',  # number of neurons in each hidden layers
+    '--network_class', 'baselines.her.actor_critic:ActorCritic',
+    '--Q_lr', '0.01',  # critic learning rate
+    '--pi_lr', '0.01',  # actor learning rate
+    '--buffer_size', str(int(1E3)),  # for experience replay
+    '--polyak', '0.95',  # polyak averaging coefficient
+    '--action_l2', '1.0',  # quadratic penalty on actions (before rescaling by max_u)
+    '--clip_obs', '200.',
+    '--scope', 'ddpg',  # can be tweaked for testing
+    '--relative_goals', 'False',
+    # training
+    '--n_cycles', '50',  # per epoch
+    '--rollout_batch_size', '1',  # per mpi thread
+    '--n_batches', '40',  # training batches per cycle
+    '--batch_size', '256',  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
+    '--n_test_rollouts', '50',  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
+    '--test_with_polyak', 'True',  # run test episodes with the target network
+    # exploration
+    '--random_eps', '0.3',  # percentage of time a random action is taken
+    '--noise_eps', '0.2',  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
+    # HER
+    '--replay_strategy', 'future',  # supported modes: future, none
+    '--replay_k', '4',  # number of additional goals used for replay, only used if off_policy_data=future
+    # normalization
+    '--norm_eps', '1',  # epsilon used for observation normalization
+    '--norm_clip', '5',  # normalized observations are cropped to this values
+    '--save_path', 'policies/her'
+]
--- run_experiments.py
+++ run_experiments.py
@@ -3,26 +3,12 @@ import os
 #from stable_baselines.her.experiment.train import main
 from baselines.run import main
 
-from config import EXPERIMENTS, GPU_ID
+from config import DDPG_DENSE, DDPG_SPARSE, DDPG_HER_DENSE, DDPG_HER_SPARSE, GPU_ID
 import threading
 
-os.environ['OPENAI_LOG_FORMAT'] = 'stdout,log,csv,tensorboard'
-
-def launch_tensorboard(tensorboard_path):
-    os.system('tensorboard --logdir=' + tensorboard_path)
-    return
-
-
 if __name__ == '__main__':
-    print("Starting tensorboard thread.")
-    t = threading.Thread(target=launch_tensorboard, args=(["logs/"]))
-    t.start()
-
     os.environ["CUDA_VISIBLE_DEVICES"] = str(GPU_ID)
 
-    print("Starting experiments.")
-    log_counter = 0
-    for EXP in EXPERIMENTS:
-        os.environ['OPENAI_LOGDIR'] = 'logs/' + str(log_counter)
-        main(EXP)
-        log_counter += 1
+    print("Starting experiment.")
+    os.environ['OPENAI_LOGDIR'] = 'logs/DDPG_DENSE/'
+    main(DDPG_SPARSE)
