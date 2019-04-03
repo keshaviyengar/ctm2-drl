7fffa77d65cd1e60ac8f851b2c034d142dde0343
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-04-03 17:13:51 +0100

    config save policy with date.

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+
+now = datetime.now()
+dt_string = now.strftime("%Y_%m_%d_%H:%M")
+
 DDPG_DENSE = {
     'parameters': [
-        '--env', 'distal-1-continuous-dense-v0',
+        '--env', 'distal-2-continuous-dense-mm-v0',
         '--extra_import', 'ctm2_envs',
         '--alg', 'her',
         '--num_cpu', '1',
-        '--num_timesteps', str(12000),
+        '--num_timesteps', str(2.5e6),
         # env
         '--max_u', '1.',  # max absolute value of actions on different coordinates
         # ddpg
         '--layers', '2',  # number of layers in the critic/actor networks
-        '--hidden', '16',  # number of neurons in each hidden layers
+        '--hidden', '32',  # number of neurons in each hidden layers
         '--network_class', 'baselines.her.actor_critic:ActorCritic',
         '--Q_lr', '0.001',  # critic learning rate
         '--pi_lr', '0.001',  # actor learning rate
-        '--buffer_size', str(int(1E3)),  # for experience replay
+        '--buffer_size', str(int(1E6)),  # for experience replay
         '--polyak', '0.95',  # polyak averaging coefficient
         '--action_l2', '1.0',  # quadratic penalty on actions (before rescaling by max_u)
         '--clip_obs', '200.',
@@ -24,8 +29,8 @@ DDPG_DENSE = {
         '--rollout_batch_size', '1',  # per mpi thread
         '--n_batches', '40',  # training batches per cycle
         '--batch_size', '256',  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
-        '--n_test_rollouts', '50',  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
-        '--test_with_polyak', 'False',  # run test episodes with the target network
+        '--n_test_rollouts', '10',  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
+        '--test_with_polyak', 'True',  # run test episodes with the target network
         # exploration
         '--random_eps', '0.3',  # percentage of time a random action is taken
         '--noise_eps', '0.2',  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
@@ -33,10 +38,10 @@ DDPG_DENSE = {
         '--replay_strategy', 'none',  # supported modes: future, none
         '--replay_k', '4',  # number of additional goals used for replay, only used if off_policy_data=future
         # normalization
-        '--norm_eps', '1',  # epsilon used for observation normalization
+        '--norm_eps', '0.01',  # epsilon used for observation normalization
         '--norm_clip', '5',  # normalized observations are cropped to this values
         '--save_path', 'policies/ddpg_dense'],
-    'gpu_id': '0',
+    'gpu_id': '1',
     'name': 'ddpg_dense'
 }
 
@@ -126,7 +131,7 @@ HER_DENSE = {
 
 HER_SPARSE = {
     'parameters': [
-        '--env', 'distal-1-continuous-sparse-v0',
+        '--env', 'distal-2-continuous-sparse-mm-v0',
         '--extra_import', 'ctm2_envs',
         '--alg', 'her',
         '--num_cpu', '1',
@@ -135,10 +140,10 @@ HER_SPARSE = {
         '--max_u', '1.',  # max absolute value of actions on different coordinates
         # ddpg
         '--layers', '2',  # number of layers in the critic/actor networks
-        '--hidden', '16',  # number of neurons in each hidden layers
+        '--hidden', '64',  # number of neurons in each hidden layers
         '--network_class', 'baselines.her.actor_critic:ActorCritic',
-        '--Q_lr', '0.001',  # critic learning rate
-        '--pi_lr', '0.001',  # actor learning rate
+        '--Q_lr', '0.0001',  # critic learning rate
+        '--pi_lr', '0.0001',  # actor learning rate
         '--buffer_size', str(int(1E6)),  # for experience replay
         '--polyak', '0.95',  # polyak averaging coefficient
         '--action_l2', '1.0',  # quadratic penalty on actions (before rescaling by max_u)
@@ -151,7 +156,7 @@ HER_SPARSE = {
         '--n_batches', '40',  # training batches per cycle
         '--batch_size', '256',  # per mpi thread, measured in transitions and reduced to even multiple of chunk_length.
         '--n_test_rollouts', '10',  # number of test rollouts per epoch, each consists of rollout_batch_size rollouts
-        '--test_with_polyak', 'True',  # run test episodes with the target network
+        '--test_with_polyak', 'False',  # run test episodes with the target network
         # exploration
         '--random_eps', '0.3',  # percentage of time a random action is taken
         '--noise_eps', '0.2',  # std of gaussian noise added to not-completely-random actions as a percentage of max_u
@@ -159,9 +164,9 @@ HER_SPARSE = {
         '--replay_strategy', 'future',  # supported modes: future, none
         '--replay_k', '4',  # number of additional goals used for replay, only used if off_policy_data=future
         # normalization
-        '--norm_eps', '1',  # epsilon used for observation normalization
+        '--norm_eps', '0.01',  # epsilon used for observation normalization
         '--norm_clip', '5',  # normalized observations are cropped to this values
-        '--save_path', 'policies/her_sparse'],
-    'gpu_id': '0',
+        '--save_path', 'policies/' + dt_string + '/her_sparse_2'],
+    'gpu_id': '1',
     'name': 'her_sparse'
 }
