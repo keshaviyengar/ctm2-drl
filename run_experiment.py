d070baa7b9de147ea31a9083d14cb49d1d4544ec
Keshav Iyengar <keshav.iyengar94@gmail.com>
2019-04-04 17:24:45 +0100

    fixes

--- run_experiment.py
+++ run_experiment.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 from datetime import datetime
 
 from baselines.run import main
-from config import HER_SPARSE_1_V0, HER_SPARSE_2_V0, HER_SPARSE_2_V1
+from config import HER_SPARSE_1_V0, HER_SPARSE_1_V1, HER_SPARSE_2_V0, HER_SPARSE_2_V1
 
 os.environ['OPENAI_LOG_FORMAT'] = 'stdout,log,csv,tensorboard'
 now = datetime.now()
@@ -22,6 +22,11 @@ if args.experiment == HER_SPARSE_1_V0['name']:
     os.environ['OPENAI_LOGDIR'] = directory + HER_SPARSE_1_V0['name']
     main(HER_SPARSE_1_V0['parameters'])
 
+elif args.experiment == HER_SPARSE_1_V1['name']:
+    os.environ["CUDA_VISIBLE_DEVICES"] = HER_SPARSE_1_V1['gpu_id']
+    os.environ['OPENAI_LOGDIR'] = directory + HER_SPARSE_1_V1['name']
+    main(HER_SPARSE_1_V0['parameters'])
+
 elif args.experiment == HER_SPARSE_2_V0['name']:
     os.environ["CUDA_VISIBLE_DEVICES"] = HER_SPARSE_2_V0['gpu_id']
     os.environ['OPENAI_LOGDIR'] = directory + HER_SPARSE_2_V0['name']
@@ -31,11 +36,6 @@ elif args.experiment == HER_SPARSE_2_V1['name']:
     os.environ["CUDA_VISIBLE_DEVICES"] = HER_SPARSE_2_V1['gpu_id']
     os.environ['OPENAI_LOGDIR'] = directory + HER_SPARSE_2_V1['name']
     main(HER_SPARSE_2_V1['parameters'])
-
-elif args.experiment == HER_SPARSE_2_V2['name']:
-    os.environ["CUDA_VISIBLE_DEVICES"] = HER_SPARSE_2_V2['gpu_id']
-    os.environ['OPENAI_LOGDIR'] = directory + HER_SPARSE_2_V2['name']
-    main(HER_SPARSE_2_V2['parameters'])
 
 else:
     raise NameError
